2fa3dad9e63e4073c98e3c449c16e1002438f4b6
Jared Murphy <jaredcmurphy@gmail.com>
2017-05-20 15:47:03 -0400

    completes commentscontroller

--- src/controllers/comments.controller.js
+++ src/controllers/comments.controller.js
@@ -5,9 +5,36 @@ class CommentsController {
 
   init() {
     // kick off controller from here
+    this.addCommentFormListener();
   }
 
+  // iterates through each comment form and adds an eventlistener
+  // to trigger a function on form submit
+  // function should grab the imageId + comment and create a new
+  // Comment with those arguments
+  // execute the render function on that found image object to
+  // append the new comment
   addCommentFormListener() {
     // create comment form listener code here
+    this.$addCommentForm.on('submit', event => {
+      event.preventDefault();
+
+      const $form = $(event.target);
+      const $input =  $form.children('input.user-text');
+      const comment = $input.val();
+      const imageId = $form.parent()[0].dataset.id;
+
+      $input.val('');
+      this.render(comment, imageId);
+    });
+  }
+
+  // selects the appropriate ul for this comment to be added to
+  // appends the new comment element to this ul
+  // Don't try to copy the ImagesController.render function because
+  // that is implemented differently
+  render(comment, imageId){
+    const $ul = $(`#image-${imageId} ul`);
+    $ul.append(`<li>${comment}</li>`);
   }
 }
