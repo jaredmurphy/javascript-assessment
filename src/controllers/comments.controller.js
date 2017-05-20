82ec88c2a046519752f2f5aee310bdc2a31df8f9
Jared Murphy <jaredcmurphy@gmail.com>
2017-05-20 18:14:54 -0400

    finish assessment

--- src/controllers/comments.controller.js
+++ src/controllers/comments.controller.js
@@ -1,41 +1,33 @@
 class CommentsController {
   constructor() {
-    this.$addCommentForm = $('.add-comment')
+    this.$addCommentForm = $('.add-comment');
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   init() {
-    // kick off controller from here
     this.addCommentFormListener();
   }
 
-  // iterates through each comment form and adds an eventlistener
-  // to trigger a function on form submit
-  // function should grab the imageId + comment and create a new
-  // Comment with those arguments
-  // execute the render function on that found image object to
-  // append the new comment
   addCommentFormListener() {
-    // create comment form listener code here
-    this.$addCommentForm.on('submit', event => {
-      event.preventDefault();
-
-      const $form = $(event.target);
-      const $input =  $form.children('input.user-text');
-      const comment = $input.val();
-      const imageId = $form.parent()[0].dataset.id;
-
-      $input.val('');
-      this.render(comment, imageId);
-    });
+    this.$addCommentForm.on('submit', this.handleSubmit);
   }
 
-  // selects the appropriate ul for this comment to be added to
-  // appends the new comment element to this ul
-  // Don't try to copy the ImagesController.render function because
-  // that is implemented differently
-  render(comment, imageId){
-    const $ul = $(`#image-${imageId} ul`);
-    const htmlString = new Comment(comment, imageId).commentEl();
+  handleSubmit(event) {
+    event.preventDefault();
+
+    const $form = $(event.target);
+    const $input =  $form.children('input.user-text');
+    const comment = $input.val();
+    const imageId = $form.parent()[0].dataset.id;
+    const newComment = new Comment(comment, imageId);
+
+    $input.val('');
+    this.render(newComment);
+  }
+
+  render(newComment) {
+    const $ul = $(`#image-${newComment.findImage.id} ul`);
+    const htmlString = newComment.commentEl();
     $ul.append(htmlString);
   }
 }
--- src/models/comment.js
+++ src/models/comment.js
@@ -1,8 +1,5 @@
 // create Comment class here
 class Comment {
-  // should initialize with an id, image object (findImage) and
-  // commentContent (the actual text of the comment)
-  // should save new comment to Comment.all property
   constructor(comment, imageId) {
     this.commentContent = comment;
     this.imageId = imageId;
@@ -11,22 +8,15 @@ class Comment {
     Comment.all.push(this);
   }
 
-  // given an int for an image id, returns the image object with that id
-  // before return - adds current comment to image's comments property
   findImage(imageId) {
     const image = Image.all[imageId];
     image.comments.push(this);
     return image;
   }
 
-  // returns a string of html
-  // html has an li tag with an id field and shows the comment
   commentEl() {
-    return `<li id='${this.id}'>${this.commentContent}</li>`;
+    return `<li id='comment-${this.id}'>${this.commentContent}</li>`;
   }
 }
 
-// should return all of the comment objects in an array
-// a property of the Comment class
 Comment.all = [];
-
